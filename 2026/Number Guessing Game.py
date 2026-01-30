861b5fc81d8591ef14b0b433c4803580f96f98d0
Gab Morauda <pupgabrielmorauda@gmail.com>
2026-01-31 00:49:57 +0800

    Modified the Number Guessing Game to 1 Difficulty, will add more later, Finished

--- 2026/Number Guessing Game.py
+++ 2026/Number Guessing Game.py
@@ -1,48 +1,47 @@
-def number_game(a):
-    print()
-
 import random as rand
 print("-----------------------------------")
 print("Welcome to my number guessing game!")
 print("-----------------------------------")
 print('''Instructions:
-    The player must pick a difficulty from Level 1 to 3, where the player
-    will guess the number based on the range given and the number of
-    guesses that they have.\n''')
-print("""The difficulties are as follows:
-    Level 1: Range (1-10), 2 Guesses
-    Level 2: Range (1-100), 5 Guesses
-    Level 3: Range (1-500), 10 Guesses""")
-menu = 0
-while menu == 0:
+    The player must guess the number between 1 to 100,
+    within 8 guesses. The less guesses you take to guess
+    the number, the more points you get!
+      
+    Any guesses made will be final, so be careful when guessing!''')
+print("-----------------------------------")
+guessnum = rand.randint(1,100)
+print("The Computer has now chosen a number, you may now guess between 1 - 100")
+roundnum = 1
+winnum = 0
+while roundnum < 9:
     print("-----------------------------------")
-    difficulty = int(input("Please Enter the Level You Wish to Play: "))
-    print("-----------------------------------")
-
-    if difficulty == 1:
-        numguess = rand.randint(1, 10)
-        print(numguess)
-        print("The program now has chosen a number in the range of 1-10...")
-        guess = int(input("Enter Your First Guess: "))
-
-        range = abs(numguess - guess)
-
-        if range == 0:
-            print("Congrats, You Have Guessed Correctly!")
-            print("Your Number: " + str(guess))
-            print("The Computer's Number: " + str(numguess))
-        elif range <= 2:
-            print("You're Burning!")
-        elif range <= 3:
-            print("You're Getting Hot!")
-        elif range <= 5:
-            print("You're Getting Cold!")
-        elif range > 5:
-            print("You're Freezing!")
-        
-    elif difficulty == 2:
-        numguess = rand.randint(1, 100)
-    elif difficulty == 3:
-        numguess = rand.randint(1, 500)
+    print("Guess #" + str(roundnum) + "!")
+    while True:
+        guessinput = str(input("Input a Number Between 1-100: "))
+        guessstrip = guessinput.strip()
+        if guessstrip.isdigit():
+            guess = int(guessstrip)
+            break
+        else:
+            print("What You Entered Was not an Integer! Please Enter an Integer...\n")
+    
+    range = abs(guessnum - guess)
+    if range == 0:
+        winnum = 1
+        break
+    elif range <= 5:
+        print("\nYou are Burning! (5 Unit Range)")
+    elif range <= 10:
+        print("\nYou are On Fire! (10 Unit Range)")
+    elif range <=15:
+        print("\nYou're Pretty Cold! (15 Unit Range)")
     else:
-        print("INVALID Difficulty, Please Try Again.")
+        print("\nYou're Freezing! (More Than 15 Units)")
+    
+    roundnum = roundnum + 1
+
+if winnum == 1:
+    print("Congrats! You guessed correctly!")
+else:
+    print("The 8 Guesses are over! The number was " + str(guessnum))
+
